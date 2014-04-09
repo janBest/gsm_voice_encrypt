5c0144d946b6814b1a884d806cd99e39e7149bcd
unknown <nyj1991@qq.com>
2014-04-09 12:24:59 +0800

    test to fix some debugs: qpsk_s , git_a_bit ,and the function of sinc interpolation

--- qpsk_mod.c
+++ qpsk_mod.c
@@ -10,8 +10,8 @@ typedef double (*PSIGNAL_FUNC)(float i,void *input_data,int input_data_len);
 #define SAMPLING_RATE 8000 //D/A转换采样率
 #define BITS 8 // 每个采样点由几位表示
 #define UNIT_TIME (1000.0f/DATA_RATE) // 每个数据脉冲的时间
-#define PI 3.141592
-#define FREQUENCY 1200.0 
+#define PI 3.1415926
+#define FREQUENCY 1200.0 //HZ
 #define SAMPLING_INTERVAL (1000.0f/SAMPLING_RATE)
 #define T (1000.0f/FREQUENCY)
 
@@ -33,13 +33,18 @@ int da_simulator(void *input_data,const int input_data_len,double *smp_points);
 float integral(float(*fun)(float x,double *smp_points,int l),float a,float b,int n,double *smp_points,int l);
 float i_f(float t,double *smp_points,int l);
 float q_f(float t,double *smp_points,int l);
+
 ///////////////////////////
 
+float test_i(float a,float b,int n,void *in,int len);
+float test_q(float a,float b,int n,void *in,int len);
 
 
 
 void main()
 {
+
+
 
 	char ch ;
 	printf("modulate or demodulate?\n");
@@ -57,6 +62,7 @@ void main()
 
 void qpsk_modulate()
 {
+\
 	void * input_data = NULL;
 	void * output_data = NULL;
 	int input_data_len = 0;
@@ -65,7 +71,7 @@ void qpsk_modulate()
 
 	FILE *fp;
 	input_data = malloc( BLOCKSIZE );
-	fp = fopen("r.raw","wb");
+	fp = fopen("r.pcm","wb");
 
 	if(fp == NULL)
 	{
@@ -97,6 +103,11 @@ void qpsk_demodulate()
 	float b;
 	float T1;
 	float T2;
+	void *test_data;
+	int test_data_len;
+	FILE *fp;
+	FILE *fp2;
+	FILE *fp3;
 	///////////////////
 	void * input_data = NULL;
 	void * output_data = NULL;
@@ -104,12 +115,14 @@ void qpsk_demodulate()
 	int output_data_len = 0;
 	double *smp_points;
 	float t=0;	
-	FILE *fp;
-	FILE *fp2;
 	input_data = malloc( BLOCKSIZE * 20);
-
-	fp = fopen("r.raw","rb");
-	fp2 = fopen("d.txt","w");
+	//////////////////////////////////
+	test_data = malloc(BLOCKSIZE);
+	init_input_data(test_data,&test_data_len);
+	/////////////////////////////////
+	fp = fopen("r.pcm","rb");
+	fp2 = fopen("p1.txt","w");
+	fp3 = fopen("p2.txt","w"); 
 	if(fp == NULL)
 	{
 		printf("can not open the file!\n");
@@ -121,19 +134,35 @@ void qpsk_demodulate()
 	smp_points =(double *) malloc( sizeof(double) * input_data_len);
 	da_simulator(input_data,input_data_len,smp_points);
 /*
-	for(t=0;t < (input_data_len-1)*SAMPLING_INTERVAL;t+=UNIT_TIME*2)
+	for(t=0;t < UNIT_TIME*2;t+= SAMPLING_INTERVAL/8)
 	{
-		printf("%f %lf\n",t+2,sinc_interpolation(t+2,smp_points,input_data_len));
+		if((t/SAMPLING_INTERVAL) == ((int)t/SAMPLING_INTERVAL))
+		{
+		fprintf(fp2,"%f %f =====\n",t,qpsk_s(t,test_data,test_data_len));
+		fprintf(fp3,"%f %f =====\n",t,sinc_interpolation(t,smp_points,input_data_len));
+		}
+		else
+		{
+		fprintf(fp2,"%f %f \n",t,qpsk_s(t,test_data,test_data_len));
+		fprintf(fp3,"%f %f \n",t,sinc_interpolation(t,smp_points,input_data_len));
+		}
 	}
 */
+
+	
 	for(t=0;t < (input_data_len-1)*SAMPLING_INTERVAL;t+= UNIT_TIME*2)
 	{
-	//	printf("%f %lf\n",t,sinc_interpolation(t,smp_points,input_data_len));
-	//	fprintf(fp2,"%f %lf\n",t,sinc_interpolation(t,smp_points,input_data_len));
-		T1 = floor(t/T) *T; 
+
+		T1 = t; 
 		T2 = T1+T;
-		a = integral(i_f,T1,T2,100,smp_points,input_data_len);
-		b = integral(q_f,T1,T2,100,smp_points,input_data_len);
+//		a = test_i(T1,T2,100,test_data,test_data_len);
+//		b = test_q(T1,T2,100,test_data,test_data_len);
+		a = (2*integral(i_f,T1,T2,100,smp_points,input_data_len))/T;
+		b = (2*integral(q_f,T1,T2,100,smp_points,input_data_len))/T;
+//		printf("%f %f\n",a,b);
+//		fprintf(fp2,"%f %f %f\n",sinc_interpolation(T1,smp_points,input_data_len),sinc_interpolation(T2,smp_points,input_data_len),sinc_interpolation((T2+T2)/2,smp_points,input_data_len));
+//		fprintf(fp2,"%f %f %f\n",qpsk_s(T1,test_data,test_data_len),qpsk_s(T2,test_data,test_data_len),qpsk_s((T2+T1)/2,test_data,test_data_len));
+		
 		if(a>0 && b>0)
 		{
 			printf("00");
@@ -149,13 +178,38 @@ void qpsk_demodulate()
 		else if(a>0&&b<0)
 		{
 			printf("10");
-		}
-		
+		}		
 	}
+	
 	fclose(fp);
 	fclose(fp2);
+	fclose(fp3);
 }
 
+
+float test_i(float a,float b,int n,void *in,int len)
+{
+	float s,h,y;   
+	int i;   
+	s=(qpsk_s(a,in,len)*cos(2*PI*FREQUENCY*a/1000)+qpsk_s(b,in,len)*cos(2*PI*FREQUENCY*b/1000))/2;   
+	h=(b-a)/n; /*积分步长*/   
+	for(i=1;i<n;i++)    
+		s=s+qpsk_s(a+i*h,in,len)*cos(2*PI*FREQUENCY*(a+i*h)/1000);   
+	y=(2*s*h)/T;   
+	return y;/*返回积分值*/  
+}
+
+float test_q(float a,float b,int n,void *in,int len)
+{
+	float s,h,y;   
+	int i;   
+	s=(qpsk_s(a,in,len)*(-sin(2*PI*FREQUENCY*a/1000))+qpsk_s(b,in,len)*(-sin(2*PI*FREQUENCY*b/1000)))/2;   
+	h=(b-a)/n; /*积分步长*/   
+	for(i=1;i<n;i++)    
+		s=s+qpsk_s(a+i*h,in,len)*(-sin(2*PI*FREQUENCY*(a+i*h)/1000));   
+	y=(2*s*h)/T;   
+	return y;/*返回积分值*/  
+}
 
 
 
@@ -177,7 +231,7 @@ double sinc_interpolation(float t,const double *smp_points,const int l)
 			continue;
 		if(j >= l)
 			continue;
-		temp=sin((t-j*SAMPLING_INTERVAL)*PI)/(PI*(t-j*SAMPLING_INTERVAL));
+		temp=sin(((t-j*SAMPLING_INTERVAL)*PI)/SAMPLING_INTERVAL)/((PI*(t-j*SAMPLING_INTERVAL)/SAMPLING_INTERVAL));
 		res+=smp_points[j]*temp;
 	}
 	return res;
@@ -197,7 +251,7 @@ int ad_simulator(PSIGNAL_FUNC s,void *input_data,const int* input_data_len,void 
 	for(i =0; i<(*input_data_len) * UNIT_TIME; i+= SAMPLING_INTERVAL)
 	{
 		printf("%d %lf\n",len,s(i,input_data,*input_data_len));
-		fprintf(fp2,"%f %lf\n",i,s(i,input_data,*input_data_len));
+		fprintf(fp2,"%d %lf\n",len,s(i,input_data,*input_data_len));
 		tmp = pcm_code(1,s(i,input_data,*input_data_len));
 		pout[len++]=tmp;
 	}
@@ -209,12 +263,17 @@ int ad_simulator(PSIGNAL_FUNC s,void *input_data,const int* input_data_len,void 
 //
 int da_simulator(void *input_data,const int input_data_len,double *smp_points)
 {
+	FILE *fp;
 	int i=0;
 	char *pdata = (char*)input_data;
+	fp = fopen("d.txt","w");
 	for(i = 0;i < input_data_len;i+= 1)
 	{
 		smp_points[i]=pcm_decode(1,pdata[i]);
+	
+		fprintf(fp,"%d %f\n",i,smp_points[i]);
 	}
+	fclose(fp);
 	return input_data_len;	
 }
 
@@ -223,28 +282,29 @@ int da_simulator(void *input_data,const int input_data_len,double *smp_points)
 double qpsk_s(float i,void *input_data,int input_data_len)
 {
 	double phase_map[2][2]={{PI/4,3*PI/4},{7*PI/4,5*PI/4}};
-	int i1 = ((int)(i/UNIT_TIME*2)) *2;
-	int i2 = ((int)(i/UNIT_TIME*2)) *2+1;
+	int i1 = ((int)(i/(UNIT_TIME*2))) *2;
+	int i2 = ((int)(i/(UNIT_TIME*2))) *2+1;
+	
 
-	int n1 = get_a_bit((void*)input_data,i1);
-	int n2 = get_a_bit((void*)input_data,i2);
+	int a = get_a_bit((void*)input_data,i1);
+	int b = get_a_bit((void*)input_data,i2);
+	
 
-
-	return cos(2*PI*FREQUENCY*i/1000+phase_map[n1][n2]);
+	return cos(2*PI*FREQUENCY*i/1000+phase_map[a][b]);
 }
 
 
 int init_input_data(void *input_data,int *input_data_len)
 {
 	int * pdata = (int *)input_data;
-	pdata[0]=0xffffffff;
-	pdata[1]=0x00000000;
+	pdata[0]=0x12345680;
+	pdata[1]=0x00000000; 
 	pdata[2]=0xffffffff;
 	pdata[3]=0x00000000;
 	pdata[4]=0xffffffff;
 	pdata[5]=0x00000000;
 	pdata[6]=0xffffffff;
-	pdata[7]=0x00000000;
+	pdata[7]=0x000000ff;
 	*input_data_len = 8*32;
 	return *input_data_len;
 }
@@ -270,7 +330,8 @@ int get_a_bit(const void * buffer,int i)
 	const char *pdata = (char *)buffer;
 	char tmp = pdata[i/8];
 	int d = i%8;
-	return ( tmp >> d )&1;
+//	printf("%d %d\n",i,( tmp >> d )&1);
+	return ( tmp >> (7-d) )&1;
 }
 
 char pcm_code(int V,double impulse)   
@@ -307,9 +368,9 @@ double pcm_decode(int V,char data)
 	int a=0;
 	double mc;
 	//所在段落   
-	int DL=get_a_bit((void *)&data,6)*4 + get_a_bit((void *)&data,5)*2 + get_a_bit((void *)&data,4)+1;
+	int DL=get_a_bit((void *)&data,1)*4 + get_a_bit((void *)&data,2)*2 + get_a_bit((void *)&data,3)+1;
     //所在段内 
-	int DN=get_a_bit((void *)&data,3)*8 + get_a_bit((void *)&data,2)*4 + get_a_bit((void *)&data,1)*2+ get_a_bit((void *)&data,0) +1;
+	int DN=get_a_bit((void *)&data,4)*8 + get_a_bit((void *)&data,5)*4 + get_a_bit((void *)&data,6)*2+ get_a_bit((void *)&data,7) +1;
 	switch(DL)   
     {   
       case 1:      a=1;       mc=0+a*(DN-0.5);break;   
@@ -322,7 +383,7 @@ double pcm_decode(int V,char data)
       case 8:      a=64;      mc=1024+a*(DN-0.5); break;   
     }
 	 mc=mc*V/2048;   
-    if(get_a_bit((void *)&data,7) ==0)  mc=mc*(-1);
+    if(get_a_bit((void *)&data,0) ==0)  mc=mc*(-1);
 	return mc;
 }
 
